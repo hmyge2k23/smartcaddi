e1a235b1ea19997c1f5405f6af3502d56eb17ff9
hmyge2k23 <hackeriua2k21@gmail.com>
2024-08-01 01:53:40 +0000

    8 commit

--- smartcaddie/settings.py
+++ smartcaddie/settings.py
@@ -79,23 +79,27 @@ WSGI_APPLICATION = 'smartcaddie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smart_caddi',
-        'HOST': "127.0.0.1",
-        "USER" : 'root',
-        'PASSWORD' : '',
-        'PORT' : 3306
-    } 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcm1lpaj6b153r',
+        'USER': 'u7f3snat6ton4h',
+        'PASSWORD': 'pea1995d56c84adc440ea5b809f23b7568aea41b2df0589ffa536e3b27d6ea871',
+        'HOST': 'c724r43q8jp5nk.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'smart_caddi',
+#         'HOST': "127.0.0.1",
+#         "USER" : 'root',
+#         'PASSWORD' : '',
+#         'PORT' : 3306
+#     } 
+# }
 
 
 # Password validation
